2135a6097a20d82e417294c65743e37cd46c0297
om <om@DESKTOP-I236LMB>
2023-03-12 08:35:55 -0700

    second commit

---
+++ JPAWithInheritances/src/module-info.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+/**
+ * @author om
+ *
+ */
+module JPAWithInheritances {
+	
+	requires dom4j;
+	requires java.desktop;
+	requires org.hibernate.orm.core;
+	requires java.persistence;
+}
